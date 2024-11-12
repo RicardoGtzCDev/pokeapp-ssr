97caa38a1d898df95a3f84c3da562466cf3bfadd
Ricardo Gutiérrez Cañas <ricardo.gtzc.dev@gmail.com>
2024-11-12 13:59:45 -0600

    RGC:sin SSR

---
+++ src/app/view/pages/not-found/not-found-page.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'not-found-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found-page.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class NotFoundPageComponent {
+
+}
