e3a75fd807de3aa39dfbc77f4ebf327a2ad4baac
Ricardo Gutiérrez Cañas <ricardo.gtzc.dev@gmail.com>
2024-11-20 11:44:00 -0600

    RGC:build:agrega browser para karma.config.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
   process.env.CHROME_BIN = require('puppeteer').executablePath();
   config.set({
+    browsers: ['ChromeHeadless'],
     basePath: '../..',
     frameworks: ['jasmine'],
     //...
